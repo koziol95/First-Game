c53d0ba2eaf7f604d4bf8749e8ddc547f8e17f65
Pav <121645352+koziol95@users.noreply.github.com>
2023-06-21 15:51:09 +0100

    A

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RUN AWAY")
 
 UFO_WIDTH, UFO_HEIGHT = 55, 40
-ROCK_WIDTH , ROCK_HEIGH = 10, 10
+ROCK_WIDTH , ROCK_HEIGHT = 20, 20
 
 
 # colors
@@ -28,15 +28,18 @@ UFO_IMAGE = pygame.image.load(os.path.join("Assets", "UFO.png"))
 UFO = pygame.transform.scale(UFO_IMAGE, (UFO_WIDTH, UFO_HEIGHT))
 
 ROCK_IMAGE = pygame. image.load(os.path.join("Assets", "Rock.png"))
-ROCK = pygame.transform.scale(ROCK_IMAGE, (ROCK_WIDTH, ROCK_HEIGH))
+ROCK = pygame.transform.scale(ROCK_IMAGE, (ROCK_WIDTH, ROCK_HEIGHT))
 
 ###
 VEL = 5
 
+ROCK_VEL = 3
+
 
 #=====================================================================
-def draw_window(ufo):
-    WIN.blit(SPACE, (0,0))
+def draw_window(ufo, rock):
+    WIN.blit(SPACE, (0, 0))
+    WIN.blit(ROCK, (rock.x, rock.y))
 
     WIN.blit(UFO, (ufo.x, ufo.y))
 
@@ -52,10 +55,23 @@ def UFO_HANDLE_MOVMENT(keys_pressed, ufo):
     if keys_pressed[pygame.K_DOWN] and ufo.y + VEL + ufo.height < HEIGHT + 2:  # DOWN
         ufo.y += VEL
 
+def ROCK_MOVMENT(rock):
+    global ROCK_VEL
+    rock.x += ROCK_VEL
+    rock.y -= 3
+    
+
+    if rock.y + ROCK_HEIGHT  >= HEIGHT or rock.y <= 0:  # Sprawdzanie kolizji z górną i dolną krawędzią
+        rock.y *= -1*ROCK_VEL
+    if rock.x + ROCK_WIDTH >= WIDTH or rock.x <= 0:  # Sprawdzanie kolizji z lewą i prawą krawędzią
+        rock.x *= -1*ROCK_VEL
+
+    
+
 
 def main():
     ufo = pygame.Rect(WIDTH//2, HEIGHT//2 , UFO_WIDTH, UFO_HEIGHT)
-
+    rock = pygame.Rect(101, 101, ROCK_WIDTH, ROCK_HEIGHT)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -68,10 +84,11 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         UFO_HANDLE_MOVMENT(keys_pressed, ufo)
+       # ROCK_MOVMENT(rock)
         
 
     
-        draw_window(ufo)
+        draw_window(ufo, rock)
 
 if __name__ == "__main__":
     main()
